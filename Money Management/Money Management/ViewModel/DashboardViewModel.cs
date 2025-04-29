05247e0a7a81852925ae69420ad28716a1d458b1
SWehara <ar113035@fhss.sjp.ac.lk>
2025-04-29 23:12:42 +0530

    Added a Budget Window

---
+++ Money Management/Money Management/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Management.ViewModel
+{
+    
+        public class DashboardViewModel
+        {
+            public double FoodSeries { get; set; }
+            public double TravelSeries { get; set; }
+            public double BillsSeries { get; set; }
+            public double OtherSeries { get; set; }
+        }
+
+    }
+
--- Money Management/Money Management/Views/Dashboardwindow.xaml.cs
+++ Money Management/Money Management/Views/Dashboardwindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using LiveCharts;
+using LiveCharts.Wpf;
+using Money_Management.ViewModel;
 
 namespace Money_Management.Views
 {
@@ -7,29 +10,66 @@ namespace Money_Management.Views
         public DashboardWindow()
         {
             InitializeComponent();
+
+            // Create an instance of the DashboardViewModel
+            DashboardViewModel viewModel = new DashboardViewModel
+            {
+                // Initial data for the pie chart (these can be dynamic)
+                FoodSeries = 5000,    // Example value for Food
+                TravelSeries = 3000,  // Example value for Travel
+                BillsSeries = 8000,   // Example value for Bills
+                OtherSeries = 2000    // Example value for Other
+            };
+
+            // Set the DataContext for the window to this instance
+            this.DataContext = viewModel;
         }
 
-        // Income Button Click: Open the IncomeWindow and hide the DashboardWindow
         private void IncomeButton_Click(object sender, RoutedEventArgs e)
         {
-            IncomeWindow incomeWindow = new IncomeWindow(this); // Pass the current DashboardWindow instance to IncomeWindow
+            IncomeWindow incomeWindow = new IncomeWindow(this);
             incomeWindow.Show();
             this.Hide();
         }
 
-        // Expenses Button Click: Open ExpensesWindow (ensure ExpensesWindow is implemented)
         private void ExpensesButton_Click(object sender, RoutedEventArgs e)
         {
             ExpensesWindow expensesWindow = new ExpensesWindow();
             expensesWindow.Show();
+            this.Hide();
         }
 
-        // Profile Image Click: Open the Profile Window and close the DashboardWindow
-        private void ProfileImage_Click(object sender, RoutedEventArgs e)
+        private void ProfileImage_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             Profilewindow profileWindow = new Profilewindow();
             profileWindow.Show();
             this.Close();
+        }
+
+        private void AddIncome_Click(object sender, RoutedEventArgs e)
+        {
+            IncomeWindow incomeWindow = new IncomeWindow(this);
+            incomeWindow.Show();
+            this.Hide();
+        }
+
+        private void AddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            ExpensesWindow expensesWindow = new ExpensesWindow();
+            expensesWindow.Show();
+            this.Hide();
+        }
+
+        private void SetBudget_Click(object sender, RoutedEventArgs e)
+        {
+            BudgetWindow budgetWindow = new BudgetWindow();
+            budgetWindow.ShowDialog();
+        }
+
+        private void ViewReports_Click(object sender, RoutedEventArgs e)
+        {
+            ReportsWindow reportsWindow = new ReportsWindow();
+            reportsWindow.ShowDialog();
         }
     }
 }
